dd35b83fcc94f9aa977902ba304a19b607bfac2a
a2244ky-kyoko <ubuntu@ip-172-31-28-90.ap-northeast-1.compute.internal>
2022-05-02 10:09:28 +0000

    finish chap5.3.3

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,4 +10,7 @@ class StaticPagesController < ApplicationController
 
   def help
   end
+  
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   #rootになると/が#になる
-  get 'static_pages/help'
+  #get 'static_pages/help'
   # Static_pages#helpと同じ
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #root 'application#hello'
-  get 'static_pages/home'
-  get 'static_pages/about'
+  #get 'static_pages/home'
+  #get 'static_pages/about'
+  #get 'static_pages/contact'
+  
+  #Chapter5で書き換えた
+  # /はパスを意味する。#は後ろがアクションであることを示す。
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
 
 end
