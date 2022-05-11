b5bcc4b36865c2c5a91ef23220f71278536781b9
a2244ky-kyoko <ubuntu@ip-172-31-28-90.ap-northeast-1.compute.internal>
2022-05-11 15:44:33 +0000

    finish chapter07

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,38 @@
 class UsersController < ApplicationController
   def new
   end
+  
+  # GET /users/:id
+  def show
+    @user = User.find(params[:id])
+    #debugger
+  end
+  
+  # GET /users/new
+  def new
+    @user = User.new
+  end
+  
+  #POST /users(+params)
+  def create
+    @user = User.new(user_params)
+    # あたらしいユーザーの保存が成功するか否か
+    if @user.save
+      # リダイレクトするまえにフラッシュする
+      flash[:success] = "Welcome to the Sample App!"
+      # GET /users/#{@user.id}
+      redirect_to @user
+      # redirect_to user_path(@user)
+      # redirect_to user_path(@user.id)
+      # redirect_to user_path(1) -> /user/1
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  # ここより下のソースコードは外側からアクセスできない（しない）と思いましょう
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid signup information" do
+    #　サインアップページにいきます（users/new）
+    get signup_path
+    # このブロックの中で実行された直前と直後で「ユーザーの数」は変わらないよ
+    assert_no_difference 'User.count' do
+      # フォームにパラメータを送ります
+      post users_path, params: { user: { name:  "",
+                                         email: "user@invalid",
+                                         password:              "foo",
+                                         password_confirmation: "bar" } }
+    end
+    # 上のブロックの値は不正なので、元のサインアップページに戻りますよ
+    assert_template 'users/new'
+  end
+  
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+    end
+    # リダイレクトする前になにかテストしたいときはここに書く。
+    follow_redirect!
+    assert_template 'users/show'
+  end
+end
